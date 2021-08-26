f7dbfab6c7a10299d60064deda03d95c3188e2bf
MyUsername <emanuele.marini37@gmail.com>
2021-08-27 00:24:20 +0200

    snack6 bozza

--- php-snacks-b1/snack6.php
+++ php-snacks-b1/snack6.php
@@ -13,6 +13,41 @@
 Utilizzare questo array https://pastebin.com/CkX3680A di partenza
  e stampare tutti gli insegnanti in un rettangolo grigio e tutti i PM in un rettangolo verde.
 */
+$db = [
+      'teachers' => [
+          [
+              'name' => 'Michele',
+              'lastname' => 'Papagni'
+          ],
+          [
+              'name' => 'Fabio',
+              'lastname' => 'Forghieri'
+          ]
+      ],
+      'pm' => [
+          [
+              'name' => 'Roberto',
+              'lastname' => 'Marazzini'
+          ],
+          [
+              'name' => 'Federico',
+              'lastname' => 'Pellegrini'
+          ]
+      ]
+  ];
+
+//ciclo per dividere teachers da pm e inserisco echo con div con background --->TODO assegnare style alle classi
+
+  for ($i = 0; $i < count($db); $i++){
+        if ($db['teachers']){
+              echo "<div class='teachers'>{$db['teachers'] [$i]['name']} {$db['teachers'] [$i]['lastname']} </div>";
+        };
+
+        if ($db['pm']){
+            echo "<div class='pm'>{$db['pm'] [$i]['name']} {$db['pm'] [$i]['lastname']} </div>";
+      };
+
+  }
 
 
 ?>
