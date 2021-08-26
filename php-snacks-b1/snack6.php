0b65ba63a3f2ef7c115d640a045cf9a1e431d35c
MyUsername <emanuele.marini37@gmail.com>
2021-08-26 18:03:40 +0200

    commit

---
+++ php-snacks-b1/snack6.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+</head>
+<body>
+
+<?php
+/*
+Utilizzare questo array https://pastebin.com/CkX3680A di partenza
+ e stampare tutti gli insegnanti in un rettangolo grigio e tutti i PM in un rettangolo verde.
+*/
+
+
+?>
+      
+</body>
+</html>
